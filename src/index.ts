8b579a8c5ee0f0dae9fc210d228eb12955b8a806
Doug Zwick <modusponens@gmail.com>
2024-06-05 18:05:44 -0700

    Basic test project to confirm things are working right

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import p5 from 'p5';
+
+const sketch = (p: p5) =>
+{
+  let x = 200, y = 200;
+  let diameter = 50;
+  let xSpeed = 2, ySpeed = 2.5;
+
+  p.setup = () =>
+  {
+    p.createCanvas(400, 400);
+  };
+  
+  p.draw = () =>
+  {
+    p.background(220);
+
+    x += xSpeed;
+    y += ySpeed;
+
+    if (x < diameter / 2 || x > p.width - diameter / 2)
+      xSpeed = -xSpeed;
+    if (y < diameter / 2 || y > p.height - diameter / 2)
+      ySpeed = -ySpeed;
+
+    p.ellipse(x, y, diameter, diameter);
+  };
+};
+
+new p5(sketch);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      compress: true,
+      port: 9000,
+      open: true,
+    },
+  };
+};
